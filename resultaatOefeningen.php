4379cf27296895f842e30b9501bfbdc07e564103
leonvk <leonvk2000@gmail.com>
2019-12-18 16:41:25 +0100

    fixes

--- resultaatOefeningen.php
+++ resultaatOefeningen.php
@@ -1,9 +1,12 @@
 <?php include("header.php"); 
-$sql = "SELECT `naamOefening`, `hoeLangGedaan`, `hoeZwaarGedaan`, `waneer` FROM `oefening` WHERE `gebruikersID`= $id ORDER BY `oefeningID` ";
+$sql = "SELECT `naamOefening`, `hoeLangGedaan`, `hoeZwaarGedaan`, `waneer` FROM `oefening` WHERE `gebruikersID`= $id ORDER BY `waneer` DESC ";
 $result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc() ) {
 		 $laatste = $row["naamOefening"];
+		 $laatste2 = $row["hoeLangGedaan"];
+		 $laatste3 = $row["hoeZwaarGedaan"];
+		 $laatste4 = $row["waneer"];
 		}
 	}
 
@@ -11,7 +14,16 @@ $result = $conn->query($sql);
 
 <h1 class="titel"> <a href="http://localhost/jaar3/projectenP9/resulaten.php">Resultaten</a></h1>
 <div class="boxen-container">
-	<p>laatste meeting: <?php echo "$laatste"; ?></p>
+	<div class="meeting">
+		<p>laatste meeting: <?php echo "<table><tr><th>oefening</th><th>hoe lang/vaak</th><th>hoe zwaar/hard</th><th>waneer</th></tr>  <tr><td>$laatste</td><td>$laatste2</td><td>$laatste3</td><td>$laatste4</td></table>"; ?></p>
+	</div>
+	<table style="width:100%">
+	<tr>
+    	<th>oefening</th>
+    	<th>hoe lang/vaak</th> 
+    	<th>hoe zwaar/hard</th>
+    	<th>waneer</th>
+ 	</tr>
 
 	<?php $result = $conn->query($sql);
 	if ($result->num_rows > 0) {
@@ -20,14 +32,24 @@ $result = $conn->query($sql);
 			$hoelang = $row["hoeLangGedaan"];
 			$hoeZwaarGedaan = $row["hoeZwaarGedaan"];
 			$waneer = $row["waneer"];
-		 echo "$naamoef - DAB <br>";
+			if($hoelang == ""){
+				$hoelang = "-";
+			}
+			if($hoeZwaarGedaan == ""){
+				$hoeZwaarGedaan = "-";
+			}
+		 echo " 
+		 	<tr>
+    			<td>$naamoef</td>
+    			<td>$hoelang</td>
+    			<td>$hoeZwaarGedaan</td>
+    			<td>$waneer</td>
+  			</tr>
+  			<br>";
 		}
 	}
-
-
-
-
 	?>
+</table>
 
 
 
